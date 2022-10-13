ea1796d6ed6389a2eccceeb6c0828e0c71c1da59
developerayuva <ayushvatsa1810@gmail.com>
2022-10-13 13:10:22 +0530

    Added Sieve approach in prime or not problem

--- prime-or-not.cpp
+++ prime-or-not.cpp
@@ -39,3 +39,35 @@ int main()
   return 0;  
 
 }  
+
+
+// Efficient approach based on Sieve of Eratosthenes to check for multiple numbers if they are prime or not
+vector<bool> sieve;
+void prime(int n){
+    sieve.resize(n+1,true);
+    sieve[0]=sieve[1]=false;
+    for(int i=2;(long long)i*i<=n;++i){
+        if(sieve[i])
+            for(int j=i*i;j<=n;j+=i)
+                sieve[j]=false;
+    }
+}
+
+int main{
+  cout<<"Enter total numbers you wanted to check for prime: ";
+  int t;
+  cin>>t;
+
+  // Initialise the sieve upto the maximum number
+  prime(100005);
+  while(t--){
+    int n;
+    cin>>n;
+    if(sieve[n]==true)
+      cout<<"Number "<<n<<" is prime\n";
+    else
+      cout<<"Number "<<n<<" is not prime\n";
+  }
+
+  return 0;
+}
