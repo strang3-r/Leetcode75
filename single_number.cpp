26f6814faed455678ff0841d28525c689c303f7c
RITABRATA DEY <ritabratadey1296@gmail.com>
2022-10-13 07:46:25 +0530

    single number
    Leetcode question no. 136 using bitwise xor operator

--- single_number.cpp
+++ single_number.cpp
@@ -2,7 +2,6 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        //approach 1
         int xorresult = 0;
         for(int i =0;i<nums.size();i++){
             xorresult^=nums[i];
