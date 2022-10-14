7c0fba588ae953d442b9495b7c5a4cfa3307e96f
Mohan0006 <87588310+Mohan0006@users.noreply.github.com>
2022-10-14 21:13:54 +0530

    Added Solution to the Problem Reordered Power Of 2 Problem in CPP (Efficient approach)

---
+++ 869.Reordered_Power_of_2.cpp
@@ -0,0 +1,27 @@
+// Solution to : Reordered Power Of 2
+// https://leetcode.com/problems/reordered-power-of-2/
+
+// This Solution has very minimal runtime
+
+class Solution {
+public:
+   bool reorderedPowerOf2(int n) {
+        string s = to_string(n);
+        sort(s.begin(),s.end());
+		
+        vector<string> power;
+        for(int i=0;i<=30;i++){
+            int p = pow(2,i);
+            power.push_back(to_string(p));
+        }
+        
+        for(int i=0;i<=30;i++){
+            sort(power[i].begin(),power[i].end());
+        }
+        
+        for(int i=0;i<=30;i++){
+            if(power[i] == s ) return true;
+        }
+        return false;
+    }
+};
