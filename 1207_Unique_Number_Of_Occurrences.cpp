a367dcf9766d7b4322b6f7868da13657b042df88
Dhruv Arora <98750226+lord-benjamin@users.noreply.github.com>
2022-10-27 16:17:24 +0530

    Create 1207_Unique_Number_Of_Occurrences.cpp

---
+++ 1207_Unique_Number_Of_Occurrences.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/unique-number-of-occurrences/
+
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        bool flag{true};
+        map<int,int> m;
+        for(int i{0}; i<arr.size(); ++i){
+            m[arr.at(i)]++;
+        }
+        vector<int> count;
+        for(auto val : m){
+            count.push_back(val.second);
+        }
+        sort(count.begin(),count.end());
+        for(int i{0}; i<count.size()-1; ++i){
+            if(count.at(i) == count.at(i+1)){
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+};
